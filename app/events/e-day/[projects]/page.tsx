86f726b535f463c3954c0385938a27c6bf12ad0a
Sifeng_Ey <sunnengsen@gmail.com>
2023-10-31 12:26:27 +0700

    note

--- app/events/e-day/[projects]/page.tsx
+++ app/events/e-day/[projects]/page.tsx
@@ -2,23 +2,41 @@ import MobileProjForm from "@/components/form/mobile/project";
 import ProjectsForm from "@/components/form/pc/project/ProjectsForm";
 
 export default function page() {
-    return (
-        <div>
-            <h1 className="mt-10 mb-4 text-center text-4xl font-extrabold leading-none tracking-tight text-gray-900 md:text-5xl lg:text-6xl dark:text-white">
-                List of Projects
-            </h1>
-            <p className="mb-6 text-center text-lg font-normal text-gray-500 lg:text-xl sm:px-16 xl:px-48 dark:text-gray-400">
-                Information of Technology <br />
-                Generation 8 <br />
-                Year 3
-            </p>
+  return (
+    <div>
+      {/* <div className="flex items-center flex-col justify-center"> */}
+          <h1 className="mt-10 mb-4 text-center text-4xl font-extrabold leading-none tracking-tight text-gray-900 md:text-5xl lg:text-6xl dark:text-white">
+            List of Projects
+          </h1>
+          <div className="flex">
+      <div className="pt-10 rounded-md text-sm pl-[11rem] w-[30%]">
+              <div className="flex items-center gap-2">
+                <h1 className=" bg-green-400 w-3 h-3 text-green-400 border-2 border-black"></h1>
+                <p>Complete</p>
+              </div>
+              <div className="flex items-center gap-2">
+                <h1 className=" bg-yellow-400 w-3 h-3 text-yellow-400 border-2 border-black"></h1>
+                <p>Some teacher complete</p>
+              </div>
+              <div className="flex items-center gap-2">
+                <h1 className=" bg-white w-3 h-3 text-white border-2 border-black"></h1>
+                <p>Not yet Evaluate</p>
+              </div>
+            </div>
+          <p className="mb-6 w-[40%] text-center text-lg font-normal text-gray-500 lg:text-xl dark:text-gray-400">
+            Information of Technology <br />
+            Generation 8 <br />
+            Year 3
+          </p>
+          </div>
+      {/* </div> */}
 
-            <div className="hidden lg:block">
-                <ProjectsForm />
-            </div>
-            <div className="lg:hidden">
-                    <MobileProjForm />
-            </div>
-        </div>
-    );
+      <div className="hidden lg:block">
+        <ProjectsForm />
+      </div>
+      <div className="lg:hidden">
+        <MobileProjForm />
+      </div>
+    </div>
+  );
 }
--- components/form/pc/project/ProjectsForm.tsx
+++ components/form/pc/project/ProjectsForm.tsx
@@ -9,7 +9,7 @@ export default function ProjectsForm() {
           defaultValue="account"
           className=" w-[90%] md:w-auto shadow-2xl border-2 mx-20  rounded-xl bg-white"
         >
-          <TabsList className="flex justify-around  rounded-xl border-none h-10 mt-5 bg-white py-6">
+          <TabsList className="flex justify-around  rounded-xl border-none h-10 my-5 bg-white py-6">
             <TabsTrigger
               value="account"
               className="h-14 w-2/5 ml-5 rounded-xl border-[#014164] text-black border-2 text-md "
@@ -23,28 +23,7 @@ export default function ProjectsForm() {
               Poster
             </TabsTrigger>
           </TabsList>
-          <div className=" flex justify-end mx-10">
-                <div className="flex items-center justify-between w-full  p-2 rounded-md">
-                  <div className="flex items-center">
-                    <h1 className=" bg-green-400 w-4 h-4 text-green-400 border-2 border-black">
-                      
-                    </h1>
-                    <p className="px-5">Complete</p>
-                  </div>
-                  <div className="flex py-1 items-center">
-                    <h1 className=" bg-yellow-400 w-4 h-4 text-yellow-400 border-2 border-black">
-                      
-                    </h1>
-                    <p className="px-5">Some teacher complete</p>
-                  </div>
-                  <div className="flex items-center">
-                    <h1 className=" bg-white w-4 h-4 text-white border-2 border-black">
-                      
-                    </h1>
-                    <p className="px-5">Not yet Evaluate</p>
-                  </div>
-                </div>
-              </div>
+          
 
           <TabsContent value="account" className=" w-[90%] mt-0 md:w-auto">
             <Presentation />
