b037ab84536e1b362178421555c38f9897eed729
J3llyFi5h <lovely18012004@gmail.com>
2023-10-31 17:17:44 +0700

    page eva

--- app/events/e-day/[projects]/evaluate/page.tsx
+++ app/events/e-day/[projects]/evaluate/page.tsx
@@ -4,7 +4,7 @@ import Image from 'next/image'
 export default function evaluate() {
 
     return (
-        <main className='md:mx-[3%] mt-[3%]'>
+        <main className='md:mx-[3%] my-[5%]'>
             <div className='h-auto flex justify-center'>
                 <div
                     className="w-screen xl:w-[70%] p-6 bg-white border-2 border-gray-300 rounded-lg shadow sm:p-6 md:p-16">
