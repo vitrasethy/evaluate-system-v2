7ae6ccc8c670bf0c225bf01b684927085cf2aa32
vitrasethy <sethy.vitra@gmail.com>
2023-11-07 09:49:38 +0700

    fetch form

---
+++ app/events/projects/evaluate/fetchData.ts
@@ -0,0 +1,27 @@
+import { cookies } from "next/headers";
+
+async function getData() {
+  const cookieStore = cookies();
+  const token = cookieStore.get("access_token");
+
+  const res = await fetch(
+    "https://admin.rupp.support/events/1/evaluation-forms",
+    {
+      method: "GET",
+      headers: {
+        "Content-Type": "application/json",
+        Authorization: "Bearer " + token?.value,
+      },
+    },
+  );
+
+  if (!res.ok) {
+    throw new Error("Failed to fetch data");
+  }
+
+  return res.json();
+}
+
+export default async function fetchData() {
+  return await getData();
+}
