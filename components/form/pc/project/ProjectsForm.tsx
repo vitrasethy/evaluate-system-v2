0786fc6632f70bf3298a9c8fac36400c2cd171b1
vitrasethy <sethy.vitra@gmail.com>
2023-11-07 09:32:08 +0700

    select

--- components/form/pc/project/ProjectsForm.tsx
+++ components/form/pc/project/ProjectsForm.tsx
@@ -1,4 +1,4 @@
-import * as React from "react"
+import * as React from "react";
 
 import {
   Select,
@@ -8,23 +8,75 @@ import {
   SelectLabel,
   SelectTrigger,
   SelectValue,
-} from "@/components/ui/select"
+} from "@/components/ui/select";
 
 export default function SelectDemo() {
   return (
-    <Select>
-      <SelectTrigger className="w-[180px]">
-      <SelectValue placeholder="Apple"/>
-      </SelectTrigger>
-      <SelectContent>
-        <SelectGroup>   
-          <SelectItem value="apple">Apple</SelectItem>
-          <SelectItem value="banana">Banana</SelectItem>
-          <SelectItem value="blueberry">Blueberry</SelectItem>
-          <SelectItem value="grapes">Grapes</SelectItem>
-          <SelectItem value="pineapple">Pineapple</SelectItem>
-        </SelectGroup>
-      </SelectContent>
-    </Select>
-  )
+    <div className="flex justify-around">
+      {/* my evaluate */}
+      <div className="mx-2">
+        <Select>
+          <SelectTrigger className="w-[200px] h-14 text-md bg-[#014164] hover:bg-[#014190] text-white">
+            <SelectValue placeholder="My Evaluate" />
+          </SelectTrigger>
+          <SelectContent className=" bg-none shadow-none border-none">
+            
+              <div className="flex justify-center w-full">
+                <div className="mx-2">
+
+              <Select>
+                <SelectTrigger className="w-full h-14 bg-[#014164] hover:bg-[#014190] text-white">
+                  <SelectValue placeholder="My Evaluate" />
+                </SelectTrigger>
+                <SelectContent>
+                  <SelectGroup></SelectGroup>
+                </SelectContent>
+              </Select>
+                </div>
+                <div className="mx2">
+
+              <Select>
+                <SelectTrigger className="w-full h-14 bg-[#014164] hover:bg-[#014190] text-white">
+                  <SelectValue placeholder="All Evaluate" />
+                </SelectTrigger>
+                <SelectContent>
+                  <SelectGroup></SelectGroup>
+                </SelectContent>
+              </Select>
+                </div>
+              </div>
+            
+          </SelectContent>
+        </Select>
+      </div>
+      {/* presentation */}
+      <div className="mx-2">
+        <Select>
+          <SelectTrigger className="w-[200px] h-14 bg-[#014164] hover:bg-[#014190] text-white">
+            <SelectValue placeholder="Presentation" />
+          </SelectTrigger>
+          <SelectContent className=" bg-none shadow-none border-none">
+           
+              <Select>
+                <SelectTrigger className="w-[200px] h-14 bg-[#014164] hover:bg-[#014190] text-white">
+                  <SelectValue placeholder="Presentation" />
+                </SelectTrigger>
+                <SelectContent>
+                  <SelectGroup></SelectGroup>
+                </SelectContent>
+              </Select>
+              <Select>
+                <SelectTrigger className="w-[200px] h-14 bg-[#014164] hover:bg-[#014190] text-white">
+                  <SelectValue placeholder="Poster" />
+                </SelectTrigger>
+                <SelectContent>
+                  <SelectGroup></SelectGroup>
+                </SelectContent>
+              </Select>
+          
+          </SelectContent>
+        </Select>
+      </div>
+    </div>
+  );
 }
