1bf49529ebee25600731fa231458e0975ee1e084
Sifeng_Ey <sunnengsen@gmail.com>
2023-11-07 09:46:36 +0700

    select

--- components/form/pc/project/ProjectsForm.tsx
+++ components/form/pc/project/ProjectsForm.tsx
@@ -12,45 +12,24 @@ import {
 
 export default function SelectDemo() {
   return (
-    <div className="flex justify-around">
+    <div className="flex justify-center">
       {/* my evaluate */}
-      <div className="mx-2">
+      <div className="mx-10">
         <Select>
-          <SelectTrigger className="w-[200px] h-14 text-md bg-[#014164] hover:bg-[#014190] text-white">
-            <SelectValue placeholder="My Evaluate" />
+          <SelectTrigger className="w-[200px] h-14 bg-[#014164] hover:bg-[#014190] text-white">
+            <SelectValue placeholder="Presentation" />
           </SelectTrigger>
-          <SelectContent className=" bg-none shadow-none border-none">
-            
-              <div className="flex justify-center w-full">
-                <div className="mx-2">
+          <SelectContent className=" ">
+           
+              <SelectItem className="h-14 bg-[#014164] hover:bg-[#014190] text-white my-2" value="my evaluate">My Evaluate</SelectItem>
+              <SelectItem className="h-14 bg-[#014164] hover:bg-[#014190] text-white" value="all evaluate">All Evaluate</SelectItem>
 
-              <Select>
-                <SelectTrigger className="w-full h-14 bg-[#014164] hover:bg-[#014190] text-white">
-                  <SelectValue placeholder="My Evaluate" />
-                </SelectTrigger>
-                <SelectContent>
-                  <SelectGroup></SelectGroup>
-                </SelectContent>
-              </Select>
-                </div>
-                <div className="mx2">
-
-              <Select>
-                <SelectTrigger className="w-full h-14 bg-[#014164] hover:bg-[#014190] text-white">
-                  <SelectValue placeholder="All Evaluate" />
-                </SelectTrigger>
-                <SelectContent>
-                  <SelectGroup></SelectGroup>
-                </SelectContent>
-              </Select>
-                </div>
-              </div>
-            
+          
           </SelectContent>
         </Select>
       </div>
       {/* presentation */}
-      <div className="mx-2">
+      <div className="mx-10">
         <Select>
           <SelectTrigger className="w-[200px] h-14 bg-[#014164] hover:bg-[#014190] text-white">
             <SelectValue placeholder="Presentation" />
