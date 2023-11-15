b1e1abe001c8630c3638af07d14bb02a257f86e1
vitrasethy <sethy.vitra@gmail.com>
2023-11-15 13:29:06 +0700

    Fix API endpoint and update project score submission.

--- app/home/[eventId]/projects/[evaluate]/action.ts
+++ app/home/[eventId]/projects/[evaluate]/action.ts
@@ -4,26 +4,24 @@ import { cookies } from "next/headers";
 import { revalidatePath } from "next/cache";
 import { redirect } from "next/navigation";
 
-
-
 export async function action(
   fetchData: any,
   projectId: any,
-  formData: FormData,
+  formData: FormData
 ) {
   const cookieStore = cookies();
   const token = cookieStore.get("access_token");
-  const eventId = cookieStore.get("event")
+  const eventId = cookieStore.get("event");
 
   const resGetUserId = await fetch(
-    "https://admin.rupp.support/auth/protected",
+    "https://admin.rupp.support/api/v1/auth/protected",
     {
       method: "GET",
       headers: {
         "Content-Type": "application/json",
         Authorization: "Bearer " + token?.value,
       },
-    },
+    }
   );
 
   if (!resGetUserId.ok) {
@@ -32,32 +30,36 @@ export async function action(
 
   const userDetail = await resGetUserId.json();
 
-  // criteria_id: cri.id,
-          // score: formData.get(cri.name),
-
   const data = {
     project_id: projectId?.evaluate,
     committee_id: userDetail.user_id,
     comment: formData.get("comment"),
-    result: [
-      fetchData.category.map((e: any) => ({
+    result: [].concat(
+      ...fetchData.map((e: any) => ({
         criteria_id: e.id,
-        score: formData.get(e.name)
-      })),
-    ],
+        score: formData.get(e.name),
+      }))
+    ),
   };
 
-  console.log(data.result)
+  console.log(data.result);
 
-  const res = await fetch(`https://admin.rupp.support/events/${eventId?.value}/project-scores`, {
-    method: "POST",
-    cache: "no-store",
-    headers: {
-      "Content-Type": "application/json",
-      Authorization: "Bearer " + token?.value,
-    },
-    body: JSON.stringify(data),
-  });
+  const res = await fetch(
+    `https://admin.rupp.support/api/v1/events/${eventId?.value}/project-scores`,
+    {
+      method: "POST",
+      cache: "no-store",
+      headers: {
+        "Content-Type": "application/json",
+        Authorization: "Bearer " + token?.value,
+      },
+      body: JSON.stringify(data),
+    }
+  );
+
+  if (!res.ok) {
+    throw new Error("Failed to fetch data");
+  }
 
   revalidatePath("/");
   redirect(`/home/events`);
